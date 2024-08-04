134465166ff92590418d0e9faf06c69030ac9766
pank3dF <m.zhilevsky@yandex.ru>
2024-08-04 15:50:01 +0300

    ready deploy

--- fsd/Entities/Posts/Ui/inputs/Input.tsx
+++ fsd/Entities/Posts/Ui/inputs/Input.tsx
@@ -1,10 +1,9 @@
-import { Input, Textarea } from "@mantine/core";
+import { Input, MantineSize, Textarea } from "@mantine/core";
 import { FieldProps } from "formik";
 
-export const MyInput: React.FC<FieldProps & React.InputHTMLAttributes<HTMLInputElement>> = ({ field, form, ...props }) => (
+export const MyInput: React.FC<FieldProps & React.InputHTMLAttributes<HTMLInputElement> & { size?: MantineSize | undefined }> = ({ field, form, ...props }) => (
     <Input {...field} {...props} />
 );
-
 export const MyTextarea: React.FC<FieldProps & React.TextareaHTMLAttributes<HTMLTextAreaElement>> = ({ field, form, ...props }) => (
     <Textarea {...field} {...props} />
 );
